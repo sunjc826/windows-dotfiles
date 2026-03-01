5d4d0af7136d67976eb038f7d61f6a94b0296f33
Sun Jiacheng <sunjc826@gmail.com>
2026-03-01 11:56:09 +0800

    Add functions for creating environment variables

--- install.ps1
+++ install.ps1
@@ -26,19 +26,19 @@ Ensures that a directory exists, creating it if necessary.
 .DESCRIPTION
 This helper checks whether the given path exists.  If it does not it will
 create the directory structure using New-Item.  No error is thrown if the
-directory already exists.
+directory already exists. Also creates the parents if missing like mkdir -p.
 
 .PARAMETER path
 The full filesystem path of the directory to verify or create.
 #>
     param([string]$path)
     if (-not (Test-Path $path)) {
-        Write-Host "Creating directory $path"
-        New-Item -ItemType Directory -Path $path | Out-Null
+        Write-Debug "Creating directory $path"
+        New-Item -ItemType Directory -Path $path -Force | Out-Null
     }
 }
 
-function Link-DotfilesItem {
+function New-DotfilesLinkItem {
 <#
 .SYNOPSIS
 Ensure a symbolic link exists at the destination pointing at the source.
@@ -77,7 +77,7 @@ Whether destRel is an absolute path.
             'SymbolicLink' {
                 $target = $item.Target
                 if ($target -eq $src) {
-                    Write-Host "Existing symlink already correct: $dest -> $src"
+                    Write-Debug "Existing symlink already correct: $dest -> $src"
                     return
                 } else {
                     throw "$dest is a symlink but points to $target instead of $src. Please fix manually."
@@ -86,7 +86,7 @@ Whether destRel is an absolute path.
             'Junction' {
                 $target = $item.Target
                 if ($target -eq $src) {
-                    Write-Host "Existing junction already correct: $dest -> $src"
+                    Write-Debug "Existing junction already correct: $dest -> $src"
                     return
                 } else {
                     throw "$dest is a junction but points to $target instead of $src. Please fix manually."
@@ -98,7 +98,7 @@ Whether destRel is an absolute path.
         }
     }
 
-    Write-Host "Creating symbolic link $dest -> $src"
+    Write-Debug "Creating symbolic link $dest -> $src"
     # Admin privileges are needed for this, the alternative is to New-Item -ItemType SymbolicLink -Path ($dest).Parent -Target ($src).Parent | Out-Null
     try {
         New-Item -ItemType SymbolicLink -Path $dest -Target $src | Out-Null
@@ -140,7 +140,7 @@ Whether destRel is an absolute path.
     $dest = Resolve-DotfilesDest $destRel $isAbsolute
     
     New-DotfilesDirectoryItem (Split-Path $dest -Parent)
-    Write-Host "Copying $src to $dest"
+    Write-Debug "Copying $src to $dest"
     Copy-Item -Path $src -Destination $dest -Force
 }
 
@@ -184,12 +184,117 @@ The directive to prefix the source path with; defaults to `source`.
     $line = "$keyword $src"
     if (Test-Path $dest) {
         if (Select-String -Path $dest -Pattern [regex]::Escape($line) -Quiet) {
-            Write-Host "Destination already contains '$line', skipping."
+            Write-Debug "Destination already contains '$line', skipping."
             return
         }
     }
-    Write-Host "Appending line '$line' to $dest"
+    Write-Debug "Appending line '$line' to $dest"
     Add-Content -Path $dest -Value $line
+}
+
+# $hkcuFound = $false
+# $hkcu = Get-PSProvider | Where-Object Name -Eq Registry | Select-Object -ExpandProperty Drives | Where-Object Root -Eq HKEY_CURRENT_USER | Select-Object Name
+
+# if ($hkcu.GetType().Name -eq 'String') {
+#     $hkcuFound = $true
+# }
+# elseif ($null -eq $hkcu) {
+#     Write-Error 'Cannot find HKCU registry'
+# }
+# elseif ($hkcu.GetType().IsArray) {
+#     Write-Error 'Multiple entries for HKEY_CURRENT_USER found'
+# }
+
+function Add-DotfilesUserPathItem {
+<#
+.SYNOPSIS
+Append an item to the user registry path, if not already present.
+If not absolute, then it will relative to `$HOME`.
+
+.PARAMETER Path
+Path name.
+
+.PARAMETER IsAbsolute
+Whether Path is an absolute path.
+#>
+    param(
+        [string]$path,
+        [bool]$isAbsolute = $false,
+        [bool]$isRegistry = $true
+    )
+    $resolvedPath = Resolve-DotfilesDest $path $isAbsolute
+    if (!$isRegistry) {
+        $currentUserPath = ${env:PATH}
+    }
+    else {
+        $currentUserPath = Get-ItemProperty HKCU:\Environment -Name Path | Select-Object -ExpandProperty Path
+    }
+
+    if ($currentUserPath -split ';' -contains $resolvedPath) {
+        Write-Debug "$path already exists in user path"
+    }
+
+    $nextPathValue = $currentUserPath + ";$resolvedPath"
+    if (!$isRegistry) {
+        ${env:PATH} = $nextPathValue   
+    } else {
+        Set-ItemProperty HKCU:\Environment -Name Path -Value $nextPathValue
+    }
+}
+
+function Set-DotfilesUserEnvironmentItem {
+<#
+.SYNOPSIS
+Append an item to the user registry environment, if not already present.
+If not absolute, then it will relative to `$HOME`.
+
+.PARAMETER Path
+Path name.
+
+.PARAMETER IsAbsolute
+Whether Path is an absolute path.
+#>
+    param(
+        [string]$name,
+        [string]$value,
+        [bool]$isOverride = $false,
+        [bool]$isRegistry = $true
+    )
+    $currentValue = $null
+    if (!$isRegistry) {
+        if (Get-ChildItem Env:\ | Where-Object Name -eq $name) {
+            $currentValue = Get-Item Env:\$name
+        }
+    }
+    else {
+        if (Get-Item HKCU:\Environment | Where-Object Property -eq $name) {
+            $currentValue = Get-ItemProperty HKCU:\Environment -Name $name | Select-Object -ExpandProperty $name
+        }
+    }
+
+    if ($null -ne $currentValue) {
+        if ($currentValue -eq $value) {
+            Write-Debug "$name already set to $value"
+        }
+
+        if (!$override) {
+            throw "$name currently set to $currentValue"
+        }
+    }
+
+    Write-Debug "Setting env $name to $value"
+    if (!$isRegistry) {
+        Set-Item Env:\$name -Value $value 
+    }
+    else {
+        Set-ItemProperty HKCU:\Environment -Name $name -Value $value
+    }
+}
+
+$coreDrive = 'C'
+$dataDrive = 'C'
+if (Get-PSDrive | Where-Object Name -eq 'D') {
+    $dataDrive = 'D'
 }
 
 # declare what should happen; this mirrors the Linux style of having a list
@@ -199,6 +304,8 @@ $actions = @(
     @{ Type = 'link'; Src = '.claude\skills\powershell'; Dest = '.claude\skills\powershell' }
     @{ Type = 'link'; Src = '.claude\skills\new-skill'; Dest = '.claude\skills\new-skill' }
     @{ Type = 'link'; Src = 'Microsoft.PowerShell_profile.ps1'; Dest = $profile; IsAbsolute = $true }
+    @{ Type = 'mkdir'; Path = "${dataDrive}:\LLM"; isAbsolute = $true }
+    @{ Type = 'userEnv'; Name = 'OLLAMA_MODELS'; Value = "${dataDrive}:\LLM"; IsAbsolute = $true }
 )
 
 # track what we did for reporting
@@ -213,12 +320,22 @@ foreach ($a in $actions) {
     $statusMessage = $null
     try {
         $abs = $a.ContainsKey('IsAbsolute') -and $a.IsAbsolute
+        $override = $a.ContainsKey('IsOverride') -and $a.IsOverride
         switch ($a.Type) {
-            'link'  { Link-DotfilesItem $a.Src $a.Dest $abs }
+            'link'  { New-DotfilesLinkItem $a.Src $a.Dest $abs }
             'copy'  { Copy-DotfilesItem $a.Src $a.Dest $abs }
             'append' {
                 $kw = if ($a.ContainsKey('Keyword')) { $a.Keyword } else { 'source' }
                 Add-DotfilesSourceItem $a.Src $a.Dest -isAbsolute $abs -keyword $kw
+            }
+            'userPath' {
+                $isRegistry = $a.ContainsKey('IsRegistry') -and $a.IsRegistry
+                Add-DotfilesUserPathItem $a.Path $abs $isRegistry
+            }
+            'mkdir' { New-DotfilesDirectoryItem $a.Path $abs }
+            'userEnv' { 
+                $isRegistry = $a.ContainsKey('IsRegistry') -and $a.IsRegistry
+                Set-DotfilesUserEnvironmentItem $a.Name $a.Value $override $isRegistry
             }
             default { throw "Unknown action type: $($a.Type)" }
         }
@@ -238,9 +355,9 @@ foreach ($a in $actions) {
     $results += $result
 }
 
-Write-Host "Bootstrap complete."
+Write-Debug "Bootstrap complete."
 
 if ($results.Count -gt 0) {
-    Write-Host "`nSummary:`n"
+    Write-Debug "`nSummary:`n"
     $results | Sort-Object -Property Status
 }
